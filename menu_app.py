d76832e234d10379f15b9e3d7e267ad4d3074c51
sebastian sovier <sebastian.sovier@tgtgroup.cl>
2023-12-13 15:23:33 -0300

    ev 2

--- menu_app.py
+++ menu_app.py
@@ -4,31 +4,33 @@ def agregar_compra(monto,compras):
 
 def mostrar_compras(compras):
     if len(compras) > 0:
-        for indice,compra in compras:
-            print("compra N° ",indice + 1," Monto: ",compra)
+        contador = 1
+        for compra in compras:
+            print("compra N ",contador ," Monto: ",compra)
+            contador +=1
     else:
         print("No existen compras registradas")
 
-def mostrar_total(compras,total_gastado):
-    for compra in compras:
-        total_gastado += compra
-
-    total_gastado = format(round(total_gastado))
-    print("$ " +total_gastado)
+def mostrar_total(total_gastado):
+    print("$ " +format(int(round(total_gastado)),',').replace(",","."))
 
 def main():
     compras = []
     total_gastado = 0
-    monto = 0
     while True:
         opcion = int(input("Ingrese una opcion del menu"))
         if opcion == 1:
+            monto = int(input("Ingrese monto de la compra"))
             agregar_compra(monto,compras)
         elif opcion == 2:
             mostrar_compras(compras)
         elif opcion == 3:
-            mostrar_total(compras,total_gastado)
+            total_gastado = sum(compras)
+            mostrar_total(total_gastado)
         elif opcion == 4:
             exit()
         else:
-            print("Opcion inválida")
+            print("Opcion invalida")
+
+if __name__ == "__main__":
+    main()
